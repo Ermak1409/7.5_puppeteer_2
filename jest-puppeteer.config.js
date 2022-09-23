b73f804c9933736efa1abe6e826fe19f00929392
Ermak <ermak1409@gmail.com>
2022-09-23 19:16:13 +0300

    new

---
+++ jest-puppeteer.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  launch: {
+    slowMo: 300,
+    headless: false,
+    defaultViewport: null,
+    devtools: true,
+    args: ["--start-maximized"], //— используем максимальный размер окна браузера
+  },
+};
